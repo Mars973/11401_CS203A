b6ce3b784e4f571f404bd8d71655eaf79cac1551
Mars973 <mars35020915@gmail.com>
2025-11-24 11:19:27 +0800

    update

---
+++ AssignmentIV/CXX/hash_fn.hpp
@@ -0,0 +1,22 @@
+/*
+   ========================================
+   hash_fn.hpp — declare your hash functions
+   ========================================
+
+   Description:
+    This file contains the hash functions for integer and string keys.
+
+   Development History:
+    - 2025/11/24: Initial implementation
+
+   Developer: Pin-Hsiang Chiu <mars35020915@gmail.com>
+ */
+#ifndef HASH_FN_HPP
+#define HASH_FN_HPP
+
+#include <string>
+
+int myHashString(const std::string& str, int m);
+int myHashInt(int key, int m);
+
+#endif
